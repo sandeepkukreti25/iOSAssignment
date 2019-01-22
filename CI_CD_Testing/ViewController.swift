576de59df0a8d8c565a71057ed7d05c761b68e41
Sandeep Kukreti <sandeep.kukreti.nl@sogeti.com>
2019-01-22 14:53:48 +0100

    Initial Commit

---
+++ CI_CD_Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI_CD_Testing
+//
+//  Created by Sandeep Kukreti on 22/01/2019.
+//  Copyright © 2019 Sogeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
